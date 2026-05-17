1b10de061d33564a2d50cd7c32fef4cc74565c0c
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-05-17 13:23:10 -0300

    chore: add main activity Celo Relay

---
+++ app/src/main/java/com/relay/celo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.relay.celo
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Background listener initialization and permission requests will hook in here
+    }
+}
